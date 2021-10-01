7950e151d36368a9af4a17636ee08c3baabdd135
alexandrenavarro <alexandre.j.navarro@gmail.com>
2021-10-01 21:55:21 +0200

    Back to legacy switch

--- src/main/java/com/github/alexandrenavarro/newswitchsample/CurrencyUtils.java
+++ src/main/java/com/github/alexandrenavarro/newswitchsample/CurrencyUtils.java
@@ -3,11 +3,15 @@ package com.github.alexandrenavarro.newswitchsample;
 public class CurrencyUtils {
 
     public static String getCurrency(Currency currency) {
-        final String s = switch (currency) {
-            case USD -> "Dollar";
-            case EUR  -> "Euro";
-            default -> "";
-        };
-        return s;
+//        final String s = switch (currency) {
+//            case USD -> "Dollar";
+//            case EUR  -> "Euro";
+//            default -> "";
+//        };
+        switch (currency) {
+            case USD : return "Dollar";
+            case EUR  : return "Euro";
+            default : return  "";
+        }
     }
 }
